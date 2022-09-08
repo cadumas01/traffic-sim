4c0ef95cfbeadf864e49dadf3a6cae4f48fbdda1
colimartin <colin.martin.01@gmail.com>
2022-09-08 12:35:05 -0400

    json rendering

--- sim_ui/src/index.js
+++ sim_ui/src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import road_data from './dummy-streets.json';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function generate_road(nodes) {
+  const roads = []
+  for (const node of Object.entries(nodes)) {
+    roads.push(node)
+  }
+  return {
+    roads
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const INITIAL_STATE = generate_road(road_data.nodes.connections);
+
+const App = () => {
+  const roads = React.useState(INITIAL_STATE);
+  const widthFactor = (window.innerWidth / 2) / 10
+  const heightFactor = (window.innerHeight / 2) / 10
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Test Map" fontSize={15} />
+          {roads[0].roads.map((_, i) => (
+          <Rect
+            key={i}
+            id={roads[0].roads[i][0]}
+            x={roads[0].roads[i][1].lon * widthFactor}
+            y={roads[0].roads[i][1].lat * heightFactor}
+            width={25}
+            height={25}
+            fill="black"
+          />
+        ))}
+        </Layer>
+      </Stage>
+    );
+  };
+  
+  const container = document.getElementById('root');
+  const root = createRoot(container);
+  root.render(<App />);
+  

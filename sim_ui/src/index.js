09515186bde9ba9bccb347f21f2cba702bd2f7e1
colimartin <colin.martin.01@gmail.com>
2022-09-15 11:00:55 -0400

    working on lines

--- sim_ui/src/index.js
+++ sim_ui/src/index.js
@@ -1,7 +1,15 @@
+import Konva from 'konva';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Stage, Layer, Rect, Text } from 'react-konva';
+import { Stage, Layer, Rect, Text, Line } from 'react-konva';
 import road_data from './backend/salisbury-road-just-roads.json';
+
+/*<Line
+          points={points}
+          stroke={'red'}
+          tension={1}
+          strokeWidth={3}
+          /> */
 
 // Takes a JSON dict group (nodes) and pushes its elements into an Array
 function dict_to_array(group) {
@@ -9,27 +17,38 @@ function dict_to_array(group) {
   for (const element of Object.entries(group)) {
     array.push(element)
   }
-  return {
-    array
-  }
+  return array
 }
 
-// Takes an array of Node ids (way) and an array of nodes (nodes) and returns an array of
-// the x and y coordinates of the nodes in the way
-function way_points(nodes, way) {
-  var points = []
-  for (const node of Object.entries(way)) {
-    //console.log(nodes[0].array)
-    console.log(nodes[0].array)
-    console.log(node[1])
-    console.log(nodes[0].array.includes(node[1]))
-    if (nodes[0].array.includes(node[1])) {
-      console.log("LETS FUCKING GO")
-    }
-    console.log(node)
+// Takes array of nodes and returns HashTable of {key: Node ID, val: Coords} 
+function nodes_to_hash(array) {
+  const ht = new Map()
+  for (const node of Object.entries(array)) {
+    ht.set(node[1][0], node[1][1])
   }
-  return points;
+  return ht
 }
+
+// Takes an array of the Node IDs of a way, and a HashTable of the form {key: Node ID, val: Coords}
+// Returns an array [x1, y1, x2, y2, x3, ...] of the x and y coordinates of the nodes in the way
+function way_points(nodes_hash, way) {
+  var points = []
+  console.log(way)
+  for (let i = way.length - 1; i >= 0; i--){
+    points.push(nodes_hash.get(way[i]).lat)
+    points.push(nodes_hash.get(way[i]).lon)
+  }
+  return points
+}
+
+// Helper function I used to check if I was gathering the correct coordinates for the nodes of each way
+/* function way_array(nodes_hash, way) {
+  var points = []
+  for (let i = way.length - 1; i >= 0; i--){
+    points.push(nodes_hash.get(way[i]))
+  }
+  return points
+} */
 
 const GEN_ROADS = dict_to_array(road_data.nodes.connections)
 const GEN_ATTRS = dict_to_array(road_data.nodes.attractions)
@@ -39,32 +58,41 @@ const App = () => {
   const roads = React.useState(GEN_ROADS)
   const attrs = React.useState(GEN_ATTRS)
   const ways = React.useState(GEN_WAYS)
-  for (let i = 0; i < ways[0].array.length; i++) {
-    var points = way_points(roads, ways[0].array[i][1].node_refs)
-  }
-  const widthFactor = 1 / 2
-  const heightFactor = 1 / 2
+  const roads_hash = nodes_to_hash(roads[0])
+  const points = way_points(roads_hash, ways[0][1][1].node_refs)
+  points.map((_, i) => (
+    points[i] = points[i] * 0.5
+  ))
+  const widthFactor = 0.5
+  const heightFactor = 0.5
+
     return (
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
           <Text text="Test Map" fontSize={15} />
-          {roads[0].array.map((_, i) => (
+          <Line
+          points={points}
+          stroke="black"
+          tension={1}
+          strokeWidth={3}
+          />
+          {roads[0].map((_, i) => (
           <Rect
             key={i}
-            id={roads[0].array[i][0]}
-            x={roads[0].array[i][1].lon * widthFactor}
-            y={roads[0].array[i][1].lat * heightFactor}
+            id={roads[0][i][0]}
+            x={roads[0][i][1].lon * widthFactor}
+            y={roads[0][i][1].lat * heightFactor}
             width={5}
             height={5}
             fill="black"
           />
         ))}
-          {attrs[0].array.map((_, i) => (
+          {attrs[0].map((_, i) => (
           <Rect
             key={i}
-            id={attrs[0].array[i][0]}
-            x={attrs[0].array[i][1].lon * widthFactor}
-            y={attrs[0].array[i][1].lat * heightFactor}
+            id={attrs[0][i][0]}
+            x={attrs[0][i][1].lon * widthFactor}
+            y={attrs[0][i][1].lat * heightFactor}
             width={30}
             height={30}
             fill="red"

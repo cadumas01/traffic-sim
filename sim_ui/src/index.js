b5cb569d91237bd76cbf8c435de2a46138d46176
colimartin <colin.martin.01@gmail.com>
2022-09-15 18:02:14 -0400

    render road lines

--- sim_ui/src/index.js
+++ sim_ui/src/index.js
@@ -4,13 +4,6 @@ import { createRoot } from 'react-dom/client';
 import { Stage, Layer, Rect, Text, Line } from 'react-konva';
 import road_data from './backend/salisbury-road-just-roads.json';
 
-/*<Line
-          points={points}
-          stroke={'red'}
-          tension={1}
-          strokeWidth={3}
-          /> */
-
 // Takes a JSON dict group (nodes) and pushes its elements into an Array
 function dict_to_array(group) {
   var array = []
@@ -33,12 +26,23 @@ function nodes_to_hash(array) {
 // Returns an array [x1, y1, x2, y2, x3, ...] of the x and y coordinates of the nodes in the way
 function way_points(nodes_hash, way) {
   var points = []
-  console.log(way)
   for (let i = way.length - 1; i >= 0; i--){
-    points.push(nodes_hash.get(way[i]).lat)
     points.push(nodes_hash.get(way[i]).lon)
+    points.push(nodes_hash.get(way[i]).lat)
   }
   return points
+}
+
+function points_to_coord_array(hash, ways) {
+  var array = []
+  for (let i = 0; i < ways[0].length; i++) {
+    const points = way_points(hash, ways[0][i][1].noderefs)
+    points.map((_, i) => (
+      points[i] = points[i] * 0.5
+    ))
+    array.push(points)
+  }
+  return array
 }
 
 // Helper function I used to check if I was gathering the correct coordinates for the nodes of each way
@@ -59,23 +63,13 @@ const App = () => {
   const attrs = React.useState(GEN_ATTRS)
   const ways = React.useState(GEN_WAYS)
   const roads_hash = nodes_to_hash(roads[0])
-  const points = way_points(roads_hash, ways[0][1][1].node_refs)
-  points.map((_, i) => (
-    points[i] = points[i] * 0.5
-  ))
+  const coord_array = points_to_coord_array(roads_hash, ways)
   const widthFactor = 0.5
   const heightFactor = 0.5
-
     return (
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
           <Text text="Test Map" fontSize={15} />
-          <Line
-          points={points}
-          stroke="black"
-          tension={1}
-          strokeWidth={3}
-          />
           {roads[0].map((_, i) => (
           <Rect
             key={i}
@@ -87,15 +81,13 @@ const App = () => {
             fill="black"
           />
         ))}
-          {attrs[0].map((_, i) => (
-          <Rect
-            key={i}
-            id={attrs[0][i][0]}
-            x={attrs[0][i][1].lon * widthFactor}
-            y={attrs[0][i][1].lat * heightFactor}
-            width={30}
-            height={30}
-            fill="red"
+          {coord_array.map((_, i) => (
+          <Line
+          key={i.toString()}
+          points={coord_array[i]}
+          stroke="black"
+          tension={1}
+          strokeWidth={3}
           />
         ))}
         </Layer>

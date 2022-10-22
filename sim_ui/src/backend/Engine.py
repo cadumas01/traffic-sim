58611aed7b08a47ee170b5cf1db70f00eb1907f7
colimartin <colin.martin.01@gmail.com>
2022-10-21 23:51:23 -0400

    refactoring traveler and engine classes

--- sim_ui/src/backend/Engine.py
+++ sim_ui/src/backend/Engine.py
@@ -20,78 +20,45 @@ data = json.load(file)
 travelers = []
 network = Network(data)
 
-def __init__(self, t):
-    while t > 0:
-        gen_travelers(dict_to_array(network.nodes.attractions), 5)
-        is_at_intersection()
-        increment_travelers()
-        t -= 1
-
-# Converts JSON dict of nodes to array of nodes
-def dict_to_array(nodes):
-  array = []
-  for node in nodes:
-    array.append(node)
-  return array
-
-# Takes array of nodes, creates x travelers at random across the nodes
-def gen_travelers(nodes, x):
-    while x > 0:
-        i = random.randint(0, len(nodes) - 1)
-        origin_node = nodes[i]
-        origin_way_seg = get_way_seg(origin_node)
-        origin_intersection = find_nearest_intersection(origin_node, origin_way_seg)
-        k =  random.randint(0, len(nodes) - 1)
-        end_node = nodes[k]
-        end_way_seg = get_way_seg(end_node)
-        end_intersection = find_nearest_intersection(end_node, end_way_seg)
-        t = origin_way_seg.attractions[origin_node].t
-        # Once we have it, the fourth argument passed to Traveler will be
-        # gen_path(origin_node)
-        traveler = Traveler("Car", t, end_node, origin_way_seg, gen_path(origin_intersection, end_intersection))
-        travelers.append(traveler)
-        x -= 1
-
-def increment_travelers():
-    for traveler in travelers:
-        traveler.increment_pos()
-
-# for each traveler, check if traveler's current_node matches any of the entries
-# in the Network intersections dictionary
-# If yes, increment the traveler's path
-# If traveler is at final destination, path cannot be incremented, 
-# "increment_path()" returns 1, we delete the traveler from the model
-def is_at_intersection():
-    for traveler in travelers:
-        traveler_node = traveler.current_node
-        for intersection in network.intersections:
-            if traveler_node in network.intersections[intersection].incoming_way_segment_refs:
-                at_destination = traveler.increment_path()
-                if at_destination == 1:
+class Engine:
+    def __init__(self, t):
+        while t > 0:
+            self.gen_travelers(data["nodes"]["attractions"].keys(), 5)
+            for traveler in travelers:
+                traveler.increment()
+                if traveler.is_done == True:
                     travelers.remove(traveler)
                     del traveler
-                break
+            t -= 1
 
-def find_nearest_intersection(attraction_id, way_seg):
-    t = way_seg.attractions[attraction_id].t
-    total_t = way_seg.t_len
-    if t / total_t < 0.5:
-        return way_seg.start_ref
-    else:
-        return way_seg.end_ref
+    # Takes array of nodes, creates x travelers at random across the nodes
+    def gen_travelers(self, nodes, x):
+        while x > 0:
+            i = random.randint(0, len(nodes) - 1)
+            origin_node = nodes[i]
+            origin_way_seg = self.get_way_seg(origin_node)
+            origin_intersection = self.find_nearest_intersection(origin_node, origin_way_seg)
+            k =  random.randint(0, len(nodes) - 1)
+            end_node = nodes[k]
+            end_way_seg = self.get_way_seg(end_node)
+            end_intersection = self.find_nearest_intersection(end_node, end_way_seg)
+            t = origin_way_seg.attractions[origin_node].t
+            end_t = end_way_seg.attractions[end_node].t
+            traveler = Traveler("Car", t, end_t, origin_way_seg, network.shortest_path(origin_intersection, end_intersection), False, False)
+            travelers.append(traveler)
+            x -= 1
 
-# Loops through every road in the map. For every way_segment in every road, 
-# if the given node exists in its dictionary of nodes, return that way_segment
-def get_way_seg(trav_node):
-    for way_segment in network.way_segments["roads"]:
-        if trav_node in network.way_segments["roads"][way_segment].attractions.keys():
-            return way_segment
+    def find_nearest_intersection(attraction_id, way_seg):
+        t = way_seg.attractions[attraction_id].t
+        total_t = way_seg.t_len
+        if t / total_t < 0.5:
+            return way_seg.start_ref
+        else:
+            return way_seg.end_ref
 
-def run(t):
-    while t > 0:
-        gen_travelers(dict_to_array(data["nodes"]["attractions"]), 5)
-        is_at_intersection()
-        increment_travelers()
-        t -= 1
-
-run(5)
+    # Loops through every road in the map. For every way_segment in every road, 
+    # if the given node exists in its dictionary of nodes, return that way_segment
+    def get_way_seg(node):
+        for way_segment in network.way_segments["roads"]:
+            if node in network.way_segments["roads"][way_segment].attractions.keys():
+                return way_segment
--- sim_ui/src/backend/Traveler.py
+++ sim_ui/src/backend/Traveler.py
@@ -1,114 +1,44 @@
 class Traveler:
 
-    # Each origin node should have a property that is the way segment associated with it
-    # Are residential buildings the only origin nodes?
-    # Proposal: Maybe we don't add way_segment into the initial JSON 
-    # (which is purely for organizing the existing data)
-    # But we need a function that goes through all of the attraction nodes that are also origin nodes
-    # And determines what way_segment they fall under 
+    # Each traveler receives 
+    # mode = mode of traveler
+    # current_t = t value of traveler on its current way segment
+    # end_t = the t value of the destination node on that node's way segment
+    # current_way_segment = the way_segment it is currently on
+    # path = the array of all of the way_segments the traveler will visit from its starting to ending intersection
+    # at_destination = boolean set to false until the traveler reaches the end of its path
+    # is_done = boolean set to false until at_destination is true and traveler reaches the t value of its end node
 
-    # String mode (car, bike, pedestrian)
-    # Dict origin_node 
-    """
-    origin_node: {
-        "lat": x,
-        "lon": y,
-        "way_segment": segment (of type WaySegment)
-    }
-    """
-    # Array path of dict elements giving lon and lat of travelers location at given time step
-    # Or this could be something where we figure out which way_segments the traveler is going to travel on
-    # Then when you find out that the traveler is no longer on their current segment you can increment 
-    # 'path' and you'll see the next way_segment it needs to get onto
-    """
-    path: [
-        "01": {
-            "lon": 2,
-            "lat": 1,
-        }
-        "02": {
-            "lon": 3,
-            "lat": 1
-        }
-        "03": {
-            "lon": 4,
-            "lat": 1
-        }
-    ]
-    """
-    """
-    path: [
-        strathmore_rd_brookline_01,
-        clinton_path,
-        beacon_st_brookline_inbound_08, 
-        beacon_st_brookline_inbound_09, 
-        beacon_st_brookline_inbound_10
-    ]
-    """
-    # Potential intersection schema
-    """
-    intersections: {
-        "beacon_street_englewood_01": {
-            "lat": 4
-            "lon": 1
-            "way_segments": [
-                beacon_st_brookline_inbound_08, 
-                beacon_st_brookline_outbound_08,
-                englewood_ave_01
-            ]
-        }
-    }
-    """
-    # Should each way_segment have a property list of adjacent segments? 
-    # Or it could have a property of which intersections it's bounded by and each intersection
-    # Can have the property of which way_segments are connected to it
-
-    # Mode = str ("Car", "Bike", or "Walk")
-    # Current_node = Node 
-    def __init__(self, mode, current_node, current_way_seg, path):
+    def __init__(self, mode, current_t, end_t, current_way_seg, path, at_destination, is_done):
         self.mode = mode
-        self.current_node = current_node
+        self.current_t = current_t
+        self.end_t = end_t
         self.current_way_seg = current_way_seg
         self.path = path
+        self.at_destination = at_destination
+        self.is_done = is_done
+        self.speed = current_way_seg.maxspeed * (1 - current_way_seg.weight)
 
-    # Move traveler to node that is the next node on its current_way_seg
+    # Increment traveler t value by speed 
+    # Speed is way_seg.maxspeed * (1 - way_seg.weight)
+    # Lower weight = faster road, so a lower weight means a higher speed
     def increment_pos(self):
-        nodes = self.current_way_seg.noderefs
-        for i in range(0, len(self.current_way_seg.noderefs)):
-            if nodes[i] == current_node:
-                current_node = nodes[i + 1]
-                break
+        self.t += self.speed
 
-    # Move traveler to way_seg that is the next value of its path
-    # On success return 1
-    # If there is no next value, return 0
     def increment_path(self):
-        if self.path.next:
-            current_way_seg = self.path.next
-            return 0
+        for i in range(len(self.path)):
+            if self.path[i] == self.current_way_seg:
+                self.current_way_seg = self.path[i + 1]
+    
+    def increment(self):
+        if self.at_destination == True:
+            if self.t >= self.end_t:
+                self.is_done = True
         else:
-            return 1
-
-    # Has an origin node, gets a destination node, finds shortest path between them
-    # Finds shortest path by some witchcraft
-    """
-    def gen_path(node):
-        path = []
-        dest_node = gen_destination(node)
-        segment = node.way_segment
-        dest_segment = dest_node.way_segment
-        while (segment != dest_segment) {
-            path.append(segment)
-            intersection = segment.intersection
-            segment = intersection.way_segment
-        }
-        return path
-    """
-        
-    # Takes origin node, calculates prob of traveler going to any given destination from this origin
-    # Randomly picks a destination based on those probabilities
-    """
-    def gen_destination(node):
-        return dest_node
-    """
-        
+            if self.t + self.speed >= self.current_way_seg.t_len:
+                self.t = (self.t - self.current_way_seg.t_len) + self.speed
+                self.increment_path()
+        self.increment_pos()
+        if self.path.next is None and self.at_destination == False:
+            self.at_destination = True
+            
